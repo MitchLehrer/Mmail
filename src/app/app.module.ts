d3412000bf60a27f9f5de92ad2f99cce56867114
Mitch Lehrer <mitchell@vivvo.com>
2019-08-23 15:15:44 -0600

    Major Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
+import { LayoutComponent } from './core/layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MobileHeaderComponent } from './core/mobile-header/mobile-header.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    LayoutComponent,
+    DashboardComponent,
+    MobileHeaderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
